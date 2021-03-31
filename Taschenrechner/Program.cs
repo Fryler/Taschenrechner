e708a28c0a799a29d717fcdb7f868fa79066226a
Steffen Richter <steffen.richter.wow@gmail.com>
2021-03-31 20:04:47 +0200

    Vorbereitung zur Auslösung von Operationen über 2 Zahlen

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -8,27 +8,38 @@ namespace Taschenrechner
 {
     class Program
     {
-        static void Main(string[] args)
+        // Methode definieren in 7 Schritten
+        // (optional) Modifizierer definieren (static, public, private)
+        // Datentyp des des Rückgabewertes definieren
+        // Methodenname definieren in CamelCase-Notation
+        // Rundklammern an den Methodennamen anfügen ()
+        // Überlegen welche Parameter übergeben werden (optional diese definieren und Datentyp ergänzen)
+        // Geschweifte Klammern {!!} einfügen
+        // Methode implementieren (Anweisung in den Methodenrumpf schreiben)
+
+       static void Main(string[] args)
         {
             //Ziel: Addieren; 2 Zahlen zwischen 0 und 10. 
-            double Summand1;
-            double Summand2;
-            double Summe;
 
             Console.WriteLine("-----Taschenrechner 1.0 by Steffen Richter-----");
 
-            Console.WriteLine("Addition: Geben Sie die erste zu addierende Zahl ein und bestätigen Sie mit Enter: ");
-            Summand1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Addition: Geben Sie bitte die zweite zu addierende Zahl ein und bestätigen Sie mit Enter: ");
-            Summand2 = Convert.ToDouble(Console.ReadLine());
-            Summe = Addiere(Summand1, Summand2);
-            Console.WriteLine("Das Addition von " + Summand1 + " und " + Summand2 + " ist: " + Summe);
+
+            Console.WriteLine("Geben Sie bitte die erste Zahl ein und bestätigen Sie mit Enter: ");
+            double x = Convert.ToDouble(Console.ReadLine()); 
+            
+            Console.WriteLine("Geben Sie bitte die zweite Zahl ein und bestätigen Sie mit Enter: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double summe = Addiere(y, x);
+
+            Console.WriteLine("Die Summe beider ist: {0}", summe);
+
             WarteAufBenutzerEingabe();
         }
 
-        static double Addiere(double Summand1, double Summand2)
+        static double Addiere(double x, double y)
         {
-            double summe = Summand1 + Summand2;
+            double summe = x + y;
 
             return summe;
         }
